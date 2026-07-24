7138c159a4df5025224aedd7b43d333f73044094
Alex Zungu <alexjidiga@gmail.com>
2026-07-24 10:52:00 +0300

    Refactor GoldStraddleBot for version update and logging
    Updated bot version to v2.0-beta1 and improved logging. Refactored trading day checks and added execution gap tracking.

--- GoldStraddleBot/GoldStraddleBot.cs
+++ GoldStraddleBot/GoldStraddleBot.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using cAlgo.API;
 
 namespace cAlgo.Robots
@@ -7,13 +8,18 @@ namespace cAlgo.Robots
     public class GoldStraddleBot : Robot
     {
 
+        private const string BotVersion = "v2.0-beta1";
+
+
         #region Parameters
 
         [Parameter("Volume (Lots)", DefaultValue = 0.01)]
         public double Volume { get; set; }
 
+
         [Parameter("Stop Loss (Pips)", DefaultValue = 100)]
         public double StopLoss { get; set; }
+
 
         [Parameter("Take Profit (Pips)", DefaultValue = 400)]
         public double TakeProfit { get; set; }
@@ -34,14 +40,18 @@ namespace cAlgo.Robots
         [Parameter("Trade Monday", DefaultValue = true)]
         public bool TradeMonday { get; set; }
 
+
         [Parameter("Trade Tuesday", DefaultValue = true)]
         public bool TradeTuesday { get; set; }
+
 
         [Parameter("Trade Wednesday", DefaultValue = true)]
         public bool TradeWednesday { get; set; }
 
+
         [Parameter("Trade Thursday", DefaultValue = true)]
         public bool TradeThursday { get; set; }
+
 
         [Parameter("Trade Friday", DefaultValue = true)]
         public bool TradeFriday { get; set; }
@@ -53,24 +63,35 @@ namespace cAlgo.Robots
         #endregion
 
 
+
         #region Variables
 
         private TimeSpan _time1;
         private TimeSpan _time2;
         private TimeSpan _time3;
 
+
         private bool _executedTime1;
         private bool _executedTime2;
         private bool _executedTime3;
 
+
         private DateTime _currentDate;
+
 
         private int _totalExecutions;
         private int _successfulBuys;
         private int _successfulSells;
         private int _failedOrders;
 
+
+        private double _totalExecutionGap;
+        private double _fastestGap = double.MaxValue;
+        private double _slowestGap;
+
+
         #endregion
+
 
 
         #region Start
@@ -79,17 +100,22 @@ namespace cAlgo.Robots
         {
             _currentDate = Server.Time.Date;
 
+
             ValidateParameters();
+
 
             Timer.Start(TimeSpan.FromMilliseconds(200));
 
-            LogInfo("GoldStraddleBot v2.0-alpha3 Started");
+
+            LogInfo($"{BotVersion} Started");
             LogInfo($"Symbol: {SymbolName}");
             LogInfo($"Volume: {Volume}");
-            LogInfo($"SL: {StopLoss} TP: {TakeProfit}");
+            LogInfo($"SL: {StopLoss}");
+            LogInfo($"TP: {TakeProfit}");
         }
 
         #endregion
+
 
 
         #region Timer
@@ -98,21 +124,21 @@ namespace cAlgo.Robots
         {
             ResetDaily();
 
+
             if (!IsTradingDay())
                 return;
 
 
             CheckTradeTime(_time1, ref _executedTime1, "Time 1");
-
             CheckTradeTime(_time2, ref _executedTime2, "Time 2");
-
             CheckTradeTime(_time3, ref _executedTime3, "Time 3");
 
 
-            Debug($"Current Time: {Server.Time:HH:mm:ss}");
+            Debug($"Time {Server.Time:HH:mm:ss.fff}");
         }
 
         #endregion
+
 
 
         #region Scheduler
@@ -123,18 +149,27 @@ namespace cAlgo.Robots
                 return;
 
 
-            if (Server.Time.TimeOfDay >= tradeTime &&
-                Server.Time.TimeOfDay < tradeTime.Add(TimeSpan.FromSeconds(1)))
+            TimeSpan now = Server.Time.TimeOfDay;
+
+
+            TimeSpan windowEnd =
+                tradeTime.Add(TimeSpan.FromSeconds(30));
+
+
+            if (now >= tradeTime && now <= windowEnd)
             {
                 executed = true;
 
-                LogInfo($"{name} Triggered");
+
+                LogInfo($"{name} triggered");
+
 
                 ExecuteStraddle(name);
             }
         }
 
         #endregion
+
 
 
         #region Trading
@@ -148,34 +183,44 @@ namespace cAlgo.Robots
             }
 
 
-            double volumeUnits = Symbol.QuantityToVolumeInUnits(Volume);
+            double volumeUnits =
+                Symbol.QuantityToVolumeInUnits(Volume);
 
 
-            DateTime start = Server.Time;
+            Stopwatch timer = Stopwatch.StartNew();
 
 
-            var buy = ExecuteMarketOrder(
-                TradeType.Buy,
-                SymbolName,
-                volumeUnits,
-                "GoldStraddle_BUY",
-                StopLoss,
-                TakeProfit);
+            var buy =
+                ExecuteMarketOrder(
+                    TradeType.Buy,
+                    SymbolName,
+                    volumeUnits,
+                    $"{BotVersion}_BUY",
+                    StopLoss,
+                    TakeProfit);
 
 
-            DateTime buyTime = Server.Time;
+            double buyMilliseconds = timer.Elapsed.TotalMilliseconds;
 
 
-            var sell = ExecuteMarketOrder(
-                TradeType.Sell,
-                SymbolName,
-                volumeUnits,
-                "GoldStraddle_SELL",
-                StopLoss,
-                TakeProfit);
+            var sell =
+                ExecuteMarketOrder(
+                    TradeType.Sell,
+                    SymbolName,
+                    volumeUnits,
+                    $"{BotVersion}_SELL",
+                    StopLoss,
+                    TakeProfit);
 
 
-            DateTime sellTime = Server.Time;
+            double sellMilliseconds = timer.Elapsed.TotalMilliseconds;
+
+
+            timer.Stop();
+
+
+            double executionGap =
+                sellMilliseconds - buyMilliseconds;
 
 
             _totalExecutions++;
@@ -184,48 +229,53 @@ namespace cAlgo.Robots
             if (buy.IsSuccessful)
                 _successfulBuys++;
             else
+            {
                 _failedOrders++;
+                LogError("BUY failed: " + buy.Error);
+            }
 
 
             if (sell.IsSuccessful)
                 _successfulSells++;
             else
+            {
                 _failedOrders++;
+                LogError("SELL failed: " + sell.Error);
+            }
 
 
-            double delay =
-                (sellTime - buyTime).TotalMilliseconds;
+            _totalExecutionGap += executionGap;
+
+
+            if (executionGap < _fastestGap)
+                _fastestGap = executionGap;
+
+
+            if (executionGap > _slowestGap)
+                _slowestGap = executionGap;
+
 
 
             if (buy.IsSuccessful && sell.IsSuccessful)
             {
                 LogSuccess(
-                    $"{trigger} executed | " +
+                    $"{trigger} completed | " +
                     $"BUY OK | SELL OK | " +
-                    $"Delay {delay} ms");
-            }
-            else
-            {
-                LogError(
-                    $"{trigger} execution problem");
+                    $"Gap {executionGap:F2} ms");
             }
         }
 
         #endregion
 
 
+
         #region Validation
 
         private void ValidateParameters()
         {
-            if (!TimeSpan.TryParse(TradeTime1, out _time1))
-                LogError("Trade Time 1 invalid");
-
-            if (!TimeSpan.TryParse(TradeTime2, out _time2))
-                LogError("Trade Time 2 invalid");
-
-            if (!TimeSpan.TryParse(TradeTime3, out _time3))
-                LogError("Trade Time 3 invalid");
+            TimeSpan.TryParse(TradeTime1, out _time1);
+            TimeSpan.TryParse(TradeTime2, out _time2);
+            TimeSpan.TryParse(TradeTime3, out _time3);
 
 
             LogInfo("Parameters validated");
@@ -234,36 +284,27 @@ namespace cAlgo.Robots
         #endregion
 
 
+
         #region Trading Days
 
         private bool IsTradingDay()
         {
-            switch (Server.Time.DayOfWeek)
+            return Server.Time.DayOfWeek switch
             {
-                case DayOfWeek.Monday:
-                    return TradeMonday;
-
-                case DayOfWeek.Tuesday:
-                    return TradeTuesday;
-
-                case DayOfWeek.Wednesday:
-                    return TradeWednesday;
-
-                case DayOfWeek.Thursday:
-                    return TradeThursday;
-
-                case DayOfWeek.Friday:
-                    return TradeFriday;
-
-                default:
-                    return false;
-            }
+                DayOfWeek.Monday => TradeMonday,
+                DayOfWeek.Tuesday => TradeTuesday,
+                DayOfWeek.Wednesday => TradeWednesday,
+                DayOfWeek.Thursday => TradeThursday,
+                DayOfWeek.Friday => TradeFriday,
+                _ => false
+            };
         }
 
         #endregion
 
 
-        #region Daily Reset
+
+        #region Reset
 
         private void ResetDaily()
         {
@@ -271,17 +312,30 @@ namespace cAlgo.Robots
                 return;
 
 
+            PrintDailySummary();
+
+
             _currentDate = Server.Time.Date;
+
 
             _executedTime1 = false;
             _executedTime2 = false;
             _executedTime3 = false;
 
 
-            PrintDailySummary();
+            _totalExecutions = 0;
+            _successfulBuys = 0;
+            _successfulSells = 0;
+            _failedOrders = 0;
+
+
+            _totalExecutionGap = 0;
+            _fastestGap = double.MaxValue;
+            _slowestGap = 0;
         }
 
         #endregion
+
 
 
         #region Logging
@@ -313,16 +367,24 @@ namespace cAlgo.Robots
 
         private void PrintDailySummary()
         {
+            double average =
+                _totalExecutions > 0
+                ? _totalExecutionGap / _totalExecutions
+                : 0;
+
+
             Print("====================");
-            Print("DAILY SUMMARY");
+            Print($"{BotVersion} DAILY SUMMARY");
             Print($"Executions: {_totalExecutions}");
             Print($"BUY Success: {_successfulBuys}");
             Print($"SELL Success: {_successfulSells}");
             Print($"Errors: {_failedOrders}");
+            Print($"Average Gap: {average:F2} ms");
+            Print($"Fastest Gap: {_fastestGap:F2} ms");
+            Print($"Slowest Gap: {_slowestGap:F2} ms");
             Print("====================");
         }
 
         #endregion
-
     }
 }
